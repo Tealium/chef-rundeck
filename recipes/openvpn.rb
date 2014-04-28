52e802cba0812dbf8a86c09fcb022d4bc203d410
Jason Bain <jasonbain@gmail.com>
2014-04-28 14:30:18 -0700

    changed location of openvpn files

--- recipes/openvpn.rb
+++ recipes/openvpn.rb
@@ -16,21 +16,21 @@ directory "#{node[:rundeck][:home]}/.openvpn" do
     group node[:rundeck][:group]
   end
 
-file "#{node[:rundeck][:home]}/.openvpn/ca.crt" do
+file "/etc/rundeck/ca.crt" do
    Chef::Log.info("The ca cert to use to connect to the access servers in qa")
    source 'ca.crt'
    mode 0600
    action :create
 end
 
-file "#{node[:rundeck][:home]}/.openvpn/rundeck.crt" do
+file "/etc/rundeck/rundeck.crt" do
    Chef::Log.info("The private cert for the rundeck user to use to connect to the access servers in qa")
    source 'rundeck.crt'
    mode 0600
    action :create
 end
 
-file "#{node[:rundeck][:home]}/.openvpn/rundeck.key" do
+file "/etc/rundeck/rundeck.key" do
    Chef::Log.info("The private key to use to connect to the access servers in qa")
    source 'rundeck.key'
    mode 0600
